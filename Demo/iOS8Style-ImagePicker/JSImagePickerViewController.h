8e7e82e5941cfb61afcb3d088c56b2a93a5ff348
Kirill Pahnev <kirill.pahnev@gmail.com>
2015-04-22 20:35:31 -0500

    Changed delegate to imagePicker:didSelectImage:

--- Demo/iOS8Style-ImagePicker/JSImagePickerViewController.h
+++ Demo/iOS8Style-ImagePicker/JSImagePickerViewController.h
@@ -8,12 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@class JSImagePickerViewController ;
+@class JSImagePickerViewController;
 @protocol JSImagePickerViewControllerDelegate <NSObject>
 
-- (void)imagePickerDidSelectImage:(UIImage *)image;
+- (void)imagePicker:(JSImagePickerViewController *)imagePicker didSelectImage:(UIImage *)image;
 
 @optional
+
 - (void)imagePickerDidOpen;
 - (void)imagePickerWillOpen;
 
@@ -28,20 +29,15 @@
     __unsafe_unretained id<JSImagePickerViewControllerDelegate> delegate;
 }
 
-@property (nonatomic, assign) id<JSImagePickerViewControllerDelegate> delegate;
+@property(nonatomic, assign) id<JSImagePickerViewControllerDelegate> delegate;
 
-@property (readonly) bool isVisible;
-@property (nonatomic) NSTimeInterval animationTime;
+@property bool isVisible;
 
-@property (nonatomic, strong) UICollectionView *collectionView;
-@property (nonatomic, strong) UIButton *photoLibraryBtn;
-@property (nonatomic, strong) UIButton *cameraBtn;
-@property (nonatomic, strong) UIButton *cancelBtn;
+@property(assign, nonatomic) NSUInteger tag;
 
 - (void)showImagePickerInController:(UIViewController *)controller;
 
 - (void)showImagePickerInController:(UIViewController *)controller animated:(BOOL)animated;
-
 
 - (void)dismiss;
 
@@ -49,31 +45,16 @@
 
 @end
 
-
-
-
-
 @interface TransitionDelegate : NSObject <UIViewControllerTransitioningDelegate>
 
 @end
 
-
-
-
-
-
 @interface AnimatedTransitioning : NSObject <UIViewControllerAnimatedTransitioning>
 
-@property (nonatomic, assign) BOOL isPresenting;
+@property(nonatomic, assign) BOOL isPresenting;
 
 @end
-
-
-
 
 @interface JSPhotoCell : UICollectionViewCell
 
 @end
-
-
-
